94efe970b0390d16d4455e78dadefc6d7d28d2cf
liyanxin <liyanxin@xunlei.com>
2017-01-06 16:26:55 +0800

    1.version 0.3.10;

---
+++ Target Support Files/Pods-happy_dns_p2c/Pods-happy_dns_p2c-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_happy_dns_p2c : NSObject
+@end
+@implementation PodsDummy_Pods_happy_dns_p2c
+@end
---
+++ Target Support Files/Pods-happy_dns_p2c/Pods-happy_dns_p2c-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_happy_dns_p2cVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_happy_dns_p2cVersionString[];
+
